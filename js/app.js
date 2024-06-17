20b7e69db265257d77c854e6bed8519f5a767850
Gordon Wai Hin Kam <li23179@bristol.ac.uk>
2024-06-17 14:45:23 +0100

    Finish Local Storage

--- js/app.js
+++ js/app.js
@@ -1,70 +1,123 @@
-// Add Item
+/* Local Storage */
+let listState = [];
+
+const STATE_KEY = "TO-DO LIST";
+
+function loadState(){
+  const val = localStorage.getItem(STATE_KEY);
+  if(val === null) return [];
+  else return JSON.parse(val);
+}
+
+function saveState(obj){
+  localStorage.setItem(STATE_KEY, JSON.stringify(obj));
+}
+
+function intiState(){
+  const prevState = loadState();
+  // render list
+  const ul = document.getElementById("list");
+  for (const state of prevState){
+    const li = createListStruct(state.text, state.checked);
+    ul.appendChild(li);
+  }
+}
+
+/* Functionality of WEB TODO-LIST*/
+
+function createListStruct(inputText, checked){
+
+  // Create li tag
+  const li = document.createElement("li");
+  li.classList.add("list-item");
+
+  // Create checkbox
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.classList.add("checkbox");
+  checkbox.onchange = checkItem;
+  checkbox.checked = checked;
+
+  // Create spanText
+  const spanText = document.createElement("span");
+  spanText.classList.add("item");
+  spanText.innerText = inputText;
+  if(checkbox.checked){
+    spanText.classList.toggle("checked");
+  }
+
+  // Create deleteButton
+  const deleteButton = document.createElement("span");
+  deleteButton.classList.add("delete");
+  deleteButton.onclick = deleteItem;
+
+  // Append all tags
+  li.appendChild(checkbox);
+  li.appendChild(spanText);
+  li.appendChild(deleteButton);
+
+  // save listState
+  listState.push({
+    text : inputText,
+    checked : checkbox.checked
+  })
+  saveState(listState);
+
+
+  return li;
+}
+
+// Add item to ul
 function addItem(){
   const ul = document.getElementById("list");
   const input = document.getElementById("input");
-  const text = input.value;
 
-  // if no text, need to alert user
-  if(text === ""){
-    alert("Please add an item");
+  if(input.text === ""){
+    alert("Please enter a task");
     return;
   }
 
-  const newItem = document.createElement("li");
-  newItem.classList.add("list-item");
-
-  // Create check box
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.classList.add("checkbox");
-
-  // Create span area for text
-  const spanText = document.createElement("span");
-  spanText.classList.add("item");
-  spanText.classList.add("checked");
-  spanText.innerText = text;
-
-  // Uncheck it first
-  spanText.classList.toggle("checked");
-
-  // Create delete button
-  const deleteButton = document.createElement("span");
-  deleteButton.classList.add("delete");
-
-  newItem.appendChild(checkbox);
-  newItem.appendChild(spanText);
-  newItem.appendChild(deleteButton);
-
-  checkItem(newItem);
-  deleteItem(newItem);
+  const text = input.value;
+  const li = createListStruct(text, false);
 
   input.value = "";
-  ul.appendChild(newItem);
+  ul.appendChild(li);
 
 }
 
-// CheckItem Auxiliary Function
-function checkItem(item){
-  const checkbox = item.childNodes.item(0);
-  const spanText = item.childNodes.item(1);
-  checkbox.addEventListener("change", () =>{
-    spanText.classList.toggle("checked");
-  });
+function deleteItem(){
+  const li = this.parentElement;
+  const ul = li.parentElement;
+
+  const index = Array.from(ul.children).indexOf(li);
+
+  ul.removeChild(li);
+
+  // Delete item in list state
+  listState.splice(index, 1);
+  saveState(listState);
 }
 
-// DeleteItem Auxiliary Function
-function deleteItem(item){
-  const deleteButton = item.childNodes.item(2);
-  deleteButton.addEventListener("click", () => {
-    item.remove();
-  })
+function checkItem(){
+  const checkbox = this;
+  const li = checkbox.parentElement;
+  const spanText = li.children.item(1);
+  spanText.classList.toggle("checked");
+
+  // Check item in list state
+  const index = Array.from(li.parentElement.children).indexOf(li);
+  listState[index].checked = checkbox.checked;
+  saveState(listState);
 }
 
-const button = document.getElementById("button");
-button.addEventListener("click", addItem);
+intiState();
 
-const form = document.getElementById("input-wrapper");
-form.addEventListener("submit", (e) =>{
+// Handling add event
+const addButton = document.getElementById("button");
+addButton.addEventListener("click", addItem);
+
+// Prevent Refresh Page
+const form =document.getElementById("input-wrapper");
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
-
